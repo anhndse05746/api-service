ad0be55633081eee620f242351c64fc039c0a424
anhnd <anhnd128.dev@gmail.com>
2024-12-23 21:47:28 +0700

    udpate dev script & +error handler midleware

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,22 @@
 import 'module-alias/register';
-import express, { Request, Response } from 'express';
-import routers from './routers/routers';
 import configDotenv from 'dotenv';
 configDotenv.config();
+import express, { Request, Response } from 'express';
 
+import routers from '@/routers/routers';
+import { errorHandler } from '@/middlewares/errorHandler';
 export const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(routers);
+app.use(errorHandler);
 
-app.get('healthcheck', (req: Request, res: Response) => {
+app.get('/healthcheck', (req: Request, res: Response) => {
   res.status(200).send('API is healthy');
+});
+
+app.get('/error', (req, res) => {
+  throw new Error('Something went wrong!');
 });
 
 app.listen(PORT, () => {
